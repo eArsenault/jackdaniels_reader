75cd4a2134b587cef68850c5699d072061d8a06b
Evan Arsenault <evan.arsenault85@gmail.com>
2023-02-16 20:03:45 -0500

    Paces are inputted as strings mm:ss

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,16 @@
 import re
 
+def convert_pace(paces):
+    paces_value = paces
+    for k, time in paces_value.items():
+        time_min, time_sec = time.split(":")
+        paces_value[k] = float(time_min) + float(time_sec)/60
+    
+    return paces_value
+
 def time_eval(string, paces):
     total_time = 0
+    pace_time = convert_pace(paces)
 
     #check for repeats - this can't handle nested parentheses for now
     while "(" in string:
@@ -18,16 +27,20 @@ def time_eval(string, paces):
 
     for step in string.split("+"):
         step = step.strip()
-        total_time = total_time + int(re.findall(r'\d+', step)[-1]) * paces[step[-1]]
+        total_time = total_time + int(re.findall(r'\d+', step)[-1]) * pace_time[step[-1]]
 
     return total_time
 
 if __name__ == "__main__":
-    #simple set of paces for now
+    #use my paces for now
     paces = { 
-        "E": 7,
-        "T": 5,
-        "M": 5.5
+        "E": "6:00",
+        "T": "4:45",
+        "M": "5:20",
+        "I": "4:00",
+        "R": "3:45",
+        "J": "6:30"
     }
 
+    
     print(time_eval("2E + 10x(1T) + 2x(1E)", paces))
